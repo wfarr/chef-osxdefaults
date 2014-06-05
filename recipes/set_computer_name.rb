55a54d48f64e59540225a949604c14a09130d330
Will Farrington <wcfarrington@gmail.com>
2014-06-05 15:48:44 -0400

    Add not_if for set_computer_name

--- recipes/set_computer_name.rb
+++ recipes/set_computer_name.rb
@@ -1,13 +1,16 @@
 execute "set ComputerName" do
-    command "scutil --set ComputerName #{node['osxdefaults']['computername']}"
+  command "scutil --set ComputerName #{node['osxdefaults']['computername']}"
+  not_if  { `scutil --get ComputerName`.chomp == node['osxdefaults']['computername'] }
 end
 
 execute "set HostName" do
-    command "scutil --set HostName #{node['osxdefaults']['computername']}"
+  command "scutil --set HostName #{node['osxdefaults']['computername']}"
+  not_if  { `scutil --get HostName`.chomp == node['osxdefaults']['computername'] }
 end
 
 execute "set LocalHostName" do
-    command "scutil --set LocalHostName #{node['osxdefaults']['computername']}"
+  command "scutil --set LocalHostName #{node['osxdefaults']['computername']}"
+  not_if  { `scutil --get LocalHostName`.chomp == node['osxdefaults']['computername'] }
 end
 
 unless node.platform_version == "10.10"
