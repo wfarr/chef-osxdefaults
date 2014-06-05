e2ce59d56bb0f79c3b5b5ed53a52b3cf1007d4d9
Will Farrington <wcfarrington@gmail.com>
2014-06-05 15:43:01 -0400

    This SMB shit is gone in 10.10

--- recipes/set_computer_name.rb
+++ recipes/set_computer_name.rb
@@ -10,8 +10,10 @@ execute "set LocalHostName" do
     command "scutil --set LocalHostName #{node['osxdefaults']['computername']}"
 end
 
-osxdefaults_defaults "Set the computer name" do
-  domain 'com.apple.smb.server'
-  key 'NetBIOSName'
-  string node['osxdefaults']['computername']
+unless node.platform_version == "10.10"
+  osxdefaults_defaults "Set the computer name" do
+    domain 'com.apple.smb.server'
+    key 'NetBIOSName'
+    string node['osxdefaults']['computername']
+  end
 end
